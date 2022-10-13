2a144981aa47a75acffa450f6b0cf2f6fe2136cf
AlejitoArg <101258602+AlejitoArg@users.noreply.github.com>
2022-10-13 12:11:33 -0300

    Add files via upload

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,40 +5,72 @@ import axios, * as others from 'axios';
 
 
 export default function Home() {
-  const [query, setQuery] = useState("")
+  const [query, setQuery] = useState([])
+  const [productsAlreadyRequested, setProductsAlreadyRequested] = useState([])
+  let productsAmmount=6;
   let navigate = useNavigate();
 
-  useEffect(()=>{            
-    axios.get(`https://dummyjson.com/products`)
+  const pedirDatosProducto = (idProduct)=>{
+    let ghostList=query
+    axios.get(`https://dummyjson.com/products/${idProduct}`)
     .then(function (response) {
-        console.log(response.data)
-        setQuery(response.data)
+      ghostList.push(response.data)
     })
     .catch(function (error) {
         console.log(error)
     })
+    .then(function () {
+      console.log(ghostList)
+      setQuery(ghostList);
+    })
+  }
+
+  const checkProductsAlreadyRequested = (number) =>{
+    for(let product of productsAlreadyRequested){
+      if(number==product) return true
+    }
+    return false
+  }
+
+  useEffect(()=>{
+    for(let i=0;i<productsAmmount/2;i++){
+      let rand;
+      do{
+        rand=1+Math.floor(Math.random()*100)
+      }while(checkProductsAlreadyRequested(rand))
+      setProductsAlreadyRequested([...productsAlreadyRequested, rand])
+      pedirDatosProducto(rand)
+    }
   },[])
 
   return (
     <>
-      {/*<div className="w-screen grid place-content-center mt-24">
-        <input className="w-[300px] text-center mb-5" placeholder="Ingrese el nombre de la persona" onChange={event => setQuery(event.target.value)} />
+    {/*<div className="w-screen grid place-content-center mt-24">
+        <input className="w-[300px] text-center mb-5" placeholder="Ingrese el nombre de la query" onChange={event => setQuery(event.target.value)} />
           <div className='grid grid-cols-4'>
             {
-        Personas.filter(Persona => {
+        query.filter(query => {
           if (query === '') {
-            return Persona;
-          } else if (Persona.nombre.toLowerCase().includes(query.toLowerCase())) {
-            return Persona;
+            return query;
+          } else if (query.nombre.toLowerCase().includes(query.toLowerCase())) {
+            return query;
           }
-        }).map((Persona) => (
-          <div  key={Persona.id} onClick={() => navigate(`/detalle/${Persona.id}`)}>
-            <div className='fit text-center'>{Persona.nombre}</div>
+        }).map((query) => (
+          <div  key={query.id} onClick={() => navigate(`/detalle/${query.id}`)}>
+            <div className='fit text-center'>{query.nombre}</div>
           </div>
           ))
         }
         </div>
       </div>*/}
+      {query.map( 
+        (i)=>(
+          <>
+            <p>{i.title}</p>
+            <img src={i.images[0]}/>
+          </>
+        )
+      )}
     </>
   )
 }
